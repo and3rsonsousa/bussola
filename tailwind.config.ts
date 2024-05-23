ab257e4796242eb22410d8a84d01cee24482f29c
Anderson Sousa <and3rsonsousa@outlook.com>
2024-05-23 10:53:46 -0300

    ronded

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,7 +82,8 @@ const config = {
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)",
+				DEFAULT: "calc(var(--radius) - 4px)",
+				sm: "calc(var(--radius) - 8px)",
 			},
 			keyframes: {
 				"accordion-down": {
