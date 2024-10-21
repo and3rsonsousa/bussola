b831b24f9681d663199a52e89afcc6aafa664c7f
Chico Sousa <ochicosousa@gmail.com>
2024-10-21 09:58:02 -0300

    finished state

--- app/components/structure/Action.tsx
+++ app/components/structure/Action.tsx
@@ -209,7 +209,7 @@ export function ActionLine({
                 </div>
               )}
             </div>
-            <LikeFooter size="sm" />
+            <LikeFooter size="sm" liked={state.slug === "finished"} />
           </div>
         ) : (
           <div
