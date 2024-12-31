af7a469c24374016e3806dd416dbf2b94d16f8c6
Chico Sousa <ochicosousa@gmail.com>
2024-12-31 10:40:22 -0300

    index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,7 @@
 import { Link } from "@remix-run/react";
 import type { MetaFunction } from "@vercel/remix";
 import Button from "~/components/structure/Button";
-import DotPattern from "~/components/structure/DottedBackground";
-import Waves from "~/components/structure/waves";
+import { DotPattern, FlickeringGrid } from "~/components/structure/Backgrounds";
 
 import { Bussola } from "~/lib/helpers";
 import { cn } from "~/lib/utils";
@@ -23,7 +22,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="relative grid h-dvh w-full place-content-center overflow-hidden bg-black">
-      <DotPattern
+      {/* <DotPattern
         width={30}
         height={30}
         cx={1}
@@ -32,6 +31,14 @@ export default function Index() {
         className={cn(
           "[mask-image:linear-gradient(to_bottom_right,white,transparent,white)]",
         )}
+      /> */}
+      <FlickeringGrid
+        className="absolute top-1/2 left-1/2 z-0 size-full -translate-x-1/2 -translate-y-1/2 scale-110"
+        squareSize={96}
+        gridGap={12}
+        color="#333"
+        maxOpacity={1}
+        flickerChance={0.2}
       />
       {/* <Waves
         size={600}
@@ -39,13 +46,18 @@ export default function Index() {
         className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
       /> */}
       {/* <img src="logo.png" alt="" className="h-6" /> */}
-      <Bussola size="md" className="text-white" />
-      <div className="mt-8 text-center">
-        <Button asChild>
-          <Link to={"/dashboard"} prefetch="intent">
-            Entrar
-          </Link>
-        </Button>
+      <div className="z-10 flex flex-col items-center p-8 text-center text-white lg:px-24">
+        <Bussola size="md" className="text-white" />
+        <div className="mt-8 text-xl font-bold">
+          Sistema de Gestão de <br /> Processos da Agência CNVT®
+        </div>
+        <div className="mt-8 text-center">
+          <Button asChild>
+            <Link to={"/dashboard"} prefetch="intent">
+              Entrar
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
